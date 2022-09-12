871554cd97ad64f3a5a811e73b0242b894759b41
IMNOTXS <xs90035190@gmail.com>
2022-09-13 01:44:16 +0300

    4

---
+++ user.js
@@ -0,0 +1,25 @@
+const postListEl = document.querySelector('.post-list')
+
+
+async function onSearchChange(event){
+    const id = event.target.value
+    renderPosts(id)
+}
+async function renderPosts(userId){
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userid || id}`)
+    const postsData = await posts.json()
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('')
+
+    function postHTML(post){
+        return ` <div class="post">
+        <div class="post__title">
+          ${post.title}
+        </div>
+        <p class="post__body">
+          ${post.body}
+        </p>
+      </div>
+      `
+    }
+}
+renderPosts(id)
